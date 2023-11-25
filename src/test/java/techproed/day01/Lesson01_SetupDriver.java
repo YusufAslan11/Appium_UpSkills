ca384345dc9f87737183cbd401e3604375315506
Yusuf Aslan <117074474+YusufAslan11@users.noreply.github.com>
2023-11-25 23:44:53 +0300

    lesson01-251123

---
+++ src/test/java/techproed/day01/Lesson01_SetupDriver.java
@@ -0,0 +1,38 @@
+package techproed.day01;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Lesson01_SetupDriver {
+    @Test
+    public void testDesiredCapabilities() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName","android");
+        capabilities.setCapability("automationName","uiautomator2");
+        capabilities.setCapability("udid","emulator-5554");
+        capabilities.setCapability("app","C:\\Users\\ttyus\\Desktop\\APK\\ApiDemos-debug.apk");
+//        capabilities.setCapability("appPackage", "io.appium.android.apis");
+//        capabilities.setCapability("appActivity", "io.appium.android.apis.ApiDemos");
+        URL url = new URL("http://0.0.0.0:4723");
+        AndroidDriver driver = new AndroidDriver(url, capabilities);
+
+    }
+    @Test
+    public void testUiAutomator2Options() throws MalformedURLException {
+        UiAutomator2Options options = new UiAutomator2Options()
+                .setUdid("emulator-5554")
+                .setApp("C:\\Users\\ttyus\\Desktop\\APK\\ApiDemos-debug.apk");
+//                .setAppPackage("io.appium.android.apis")
+//                .setAppActivity("io.appium.android.apis.ApiDemos");
+        URL url = new URL("http://0.0.0.0:4723");
+
+        AndroidDriver driver = new AndroidDriver(url, options);
+    }
+
+
+}
