faa96bcf5c1e4271098890f709c87f16b5f2e70a
Yusuf Aslan <117074474+YusufAslan11@users.noreply.github.com>
2023-11-30 01:27:02 +0300

    301123

--- src/test/java/techproed/day03/Lesson06_ElementDegerleriniCekme.java
+++ src/test/java/techproed/day03/Lesson06_ElementDegerleriniCekme.java
@@ -11,23 +11,21 @@ import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-
 public class Lesson06_ElementDegerleriniCekme extends ApiDemosBaseTest {
     @Test
     public void testElementDegerleriniCekme() throws MalformedURLException {
 
         By accessibility = AppiumBy.accessibilityId("Accessibility");
 
-        System.out.println("Get Ttext : " + driver.findElement(accessibility).getText());
-        System.out.println("Get Attribute : " + driver.findElement(accessibility).getAttribute("package"));
-        System.out.println("Get Attribute : " + driver.findElement(accessibility).getAttribute("checkable"));
+        System.out.println("Get Text: " + driver.findElement(accessibility).getText());
+        System.out.println("Get Attribute: " + driver.findElement(accessibility).getAttribute("package"));
+        System.out.println("Get Attribute: " + driver.findElement(accessibility).getAttribute("checkable"));
 
-        System.out.println("isSelected : "+driver.findElement(accessibility).isSelected());
-        System.out.println("isEnabled : "+driver.findElement(accessibility).isEnabled());
-        System.out.println("isDisplayed : "+driver.findElement(accessibility).isDisplayed());
+        System.out.println("isSelected: " + driver.findElement(accessibility).isSelected());
+        System.out.println("isEnabled: " + driver.findElement(accessibility).isEnabled());
+        System.out.println("isDisplayed: " + driver.findElement(accessibility).isDisplayed());
 
-        System.out.println("Get Size : "+driver.findElement(accessibility).getSize());
-        System.out.println("Get Location : "+driver.findElement(accessibility).getLocation());
-
+        System.out.println("Get Size: " + driver.findElement(accessibility).getSize());
+        System.out.println("Get Location: " + driver.findElement(accessibility).getLocation());
     }
 }
